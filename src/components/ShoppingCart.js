610ac5a83256a08c9ebd7f8145086cada6a021a0
wlachoo <w.edgardo22@gmail.com>
2019-06-24 18:51:23 -0400

    agregacion de productos en actionCreators.js

---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Panel, Table, Button, Glyphicon } from 'react-bootstrap';
+import { removeFromCart } from '../actionCreators';
+import {connect} from 'react-redux';
+
+const styles = {
+  footer: {
+    fontWeight: 'bold'
+  }
+}
+
+
+const ShoppingCart = ({cart,removeFromCart}) => {
+
+    return (
+      <Panel header="Shopping Cart">
+        <Table fill>
+          <tbody>
+            {cart.map(product =>
+              <tr key={product.id}>
+                <td>{product.name}</td>
+                <td className="text-right">${product.price}</td>
+                <td className="text-right"><Button bsSize="xsmall" bsStyle="danger" onClick={() => removeFromCart(product)}><Glyphicon glyph="trash" /></Button></td>
+              </tr>
+            )}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan="4" style={styles.footer}>
+                Total: ${cart.reduce((sum, product) => sum + product.price, 0)}
+              </td>
+            </tr>
+          </tfoot>
+        </Table>
+
+      </Panel>
+  );
+
+}
+
+const mapStateToProps = state => {
+  return{
+    cart: state.cart
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return{
+      removeFromCart(product){
+        dispatch(removeFromCart(product));
+    }  
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCart);
